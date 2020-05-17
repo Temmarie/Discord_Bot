bb898ab54325713d41acbf6110a7fc149b1091b0
Temmarie <temmar_whyte16@yahoo.com>
2020-05-17 22:19:04 +0100

    made changes to main.rb file

--- bin/main.rb
+++ bin/main.rb
@@ -1,4 +1,11 @@
-require './lib/bot.rb'
-bot = Bot.new('token_key')
+#!/usr/bin/env ruby
+require_relative '../lib/bot.rb'
+bot = Bot.new('NzA2Nzk0MTEwMzA4MDU3MTMw.Xr55IQ.fMIcyzIlimtiar5CA1sYWW2sDnw')
 
-bot.game
+bot.guess_game
+bot.date_of_birth
+bot.more_info
+bot.current_time
+bot.compliment
+bot.sum
+bot.start
